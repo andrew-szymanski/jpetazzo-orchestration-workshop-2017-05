debc62d798d1e2372053215cf3cfd61bb3907d06
Andrew Szymanski <andrew.szymanski@unibet.com>
2017-06-01 11:41:37 +0200

    specify number of nodes

--- _sdr-poc/create-poc-cluster.sh
+++ _sdr-poc/create-poc-cluster.sh
@@ -8,3 +8,15 @@ log() {
 log "$0 starting..."
 log $(docker-machine -v)
 
+DEFAULT=1
+NODES_NUMBER=${1:-1}
+log "number of nodes: [${NODES_NUMBER}]"
+
+for N in $(seq 1 ${NODES_NUMBER}); do
+   log "   a"
+done
+
+
+
+
+
